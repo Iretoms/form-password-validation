1529de3c57049c87d048bb93f1f4aef01f213c65
iretoms <iretomiwajonathan@gmail.com>
2022-06-04 20:14:14 -0700

    see/unsee password

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const labels = document.querySelectorAll('.form-control label')
+const passwordEl = document.getElementById('password')
+const seeButton = document.querySelector('.seeBtn')
+const submitButton = document.querySelector('.btn')
 
 labels.forEach(label =>{
     label.innerHTML = label.innerText
@@ -6,5 +9,20 @@ labels.forEach(label =>{
           .map((letter, idx)=>`<span
            style="transition-delay: ${idx * 50}ms">${letter}</span>`)
           .join('')
+})
+
+seeButton.addEventListener('click',()=>{
+    const type = passwordEl.getAttribute("type") === "password" ? "text" : "password"
+    passwordEl.setAttribute("type", type)
+})
+
+passwordEl.addEventListener('input',()=>{
+    if (passwordEl.value === ''){
+        seeButton.classList.remove('show')
+    } else {
+        seeButton.classList.add('show')
+    }
+
+    checkPassword()
 })
 
